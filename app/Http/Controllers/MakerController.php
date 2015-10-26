e3477001810fc4cef60ec27806ab06b87ea6906d
kazin8 <kazin8@gmail.com>
2015-10-26 04:36:16 +0300

    Stateless auth
    - Middleware
    - Session for array

--- app/Http/Controllers/MakerController.php
+++ app/Http/Controllers/MakerController.php
@@ -13,7 +13,7 @@ class MakerController extends Controller
 {
 
     public function __construct(){
-        $this->middleware('auth.basic', ['except'=>['index','show']]);
+        $this->middleware('auth.basic.once', ['except'=>['show']]);
     }
     /**
      * Display a listing of the resource.
